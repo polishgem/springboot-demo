3d7b757f2e14ec566c366c66b2e4574b70c5aa26
wm <wm.self.e@gmail.com>
2018-11-22 15:24:23 +0800

    thymeleaf

--- src/main/java/com/wm/springboot/demo/web/HelloController.java
+++ src/main/java/com/wm/springboot/demo/web/HelloController.java
@@ -2,19 +2,29 @@ package com.wm.springboot.demo.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
+    @ResponseBody
     @RequestMapping("/hello")
-    public String index() {
+    public String hello() {
         logger.info("hello info");
         logger.error("hello error");
         return "Hello World2";
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("test", "test");
+        return "index";
     }
 
 }
