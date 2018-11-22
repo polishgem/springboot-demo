8cf8f373c0273fef3601751a270747ce9dd75769
wm <wm.self.e@gmail.com>
2018-11-22 17:13:59 +0800

    Transactional

--- src/main/java/com/wm/springboot/demo/service/UserService.java
+++ src/main/java/com/wm/springboot/demo/service/UserService.java
@@ -1,8 +1,11 @@
 package com.wm.springboot.demo.service;
 
 import com.wm.springboot.demo.domain.User;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserService {
 
+    @Transactional(propagation = Propagation.REQUIRED)
     User queryUser(long id);
 }
